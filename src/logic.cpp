ea2a3c8fc8f9ef577cfaf853ab2a401e9e4ab39d
Nomeda <st40014@stclares.ac.uk>
2026-07-16 17:47:20 +0000

    added json libraries & load tasks function

--- src/logic.cpp
+++ src/logic.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
 
+using json = nlohmann::json;
+
+const std::string TASKS = "tasks.json";
+
+json loadTasks(){
+    std::ifstream file(TASKS);
+    
+    if (!file.is_open()){
+        std::cout<<"Could not open file :(\n";
+        return json::array();
+    }
+
+    json tasks;
+    file >> tasks;
+
+    file.close();
+    
+    return tasks;
+}
+
+
+// MAINNNNN
 int main()
 {
-    std::cout<<"Welcome to the task manager!\n";
-    std::cout<<"Please select an option:\n";
+    std::cout<<"hey, Hey, HEY! WELCOME to the task manager!\n";
     std::cout<<"1. Add a task\n";
     std::cout<<"2. List tasks\n";
     std::cout<<"3. Update a task\n";
     std::cout<<"4. Delete a task\n";
-    std::cout<<"5. Quit\n";
+    std::cout<<"5. Quit :(\n";
 
-    std::cout<<"Enter your choice (1-5): \n";
+    std::cout<<"Pick ONE! (1-5):\n";
     int choice{};
     std::cin>> choice;
 
