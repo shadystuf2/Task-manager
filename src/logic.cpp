5c207e984b3813b89bf00db9e3d9ce3f5abcd3af
Nomeda <st40014@stclares.ac.uk>
2026-07-16 16:58:57 +0000

    Added initial setup

--- src/logic.cpp
+++ src/logic.cpp
@@ -2,6 +2,18 @@
 
 int main()
 {
-    std::cout<<"Hello!";
+    std::cout<<"Welcome to the task manager!\n";
+    std::cout<<"Please select an option:\n";
+    std::cout<<"1. Add a task\n";
+    std::cout<<"2. List tasks\n";
+    std::cout<<"3. Update a task\n";
+    std::cout<<"4. Delete a task\n";
+    std::cout<<"5. Quit\n";
+
+    std::cout<<"Enter your choice (1-5): \n";
+    int choice{};
+    std::cin>> choice;
+
+    std::cout<<"You selected option: "<< choice << std::endl;
     return 0;
 }
