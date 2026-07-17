366bac2667ed8b390866a51b2cf4500b452176be
Nomeda <st40014@stclares.ac.uk>
2026-07-17 09:52:52 +0000

    polished final product

--- src/logic.cpp
+++ src/logic.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <nlohmann/json.hpp>
+#include <chrono>
 
 using namespace std;
 using json = nlohmann::json;
@@ -27,50 +28,74 @@ json loadTasks(){
     return tasks;
 }
 
+
 void saveTasks(const json& tasks) {
+
     ofstream file(TASKS);
     file << tasks.dump(4); // .dump converts JSON object (compatible with cpp, not json file) to string
     file.close();
+
 }
 
 
 // LOGIC FUNCTIONS
 void addTask(json& tasks) {
-    cout << "Enter task title: \n";
-    string input_task;
-    getline(cin, input_task);
+    string title, description, due_date;
 
-    tasks.push_back(input_task); // .push_back adds element to end of json file
+    cout << "Enter task title: \n";
+    getline(cin, title);
+
+    cout << "Enter task description: \n";
+    getline(cin, description);
+
+    cout << "Enter task due date (YYYY-MM-DD): \n";
+    getline(cin, due_date);
+
+
+    json task = {
+        {"due_date", due_date},
+        {"description", description}, 
+        {"title", title}
+    };
+
+    tasks.push_back(task); // .push_back adds element to end of json file
     saveTasks(tasks);
+    cout << "Task " << task["title"] << " successfully added!\n";
     
 }
 
 
 void viewTasks(const json& tasks) {
+    
     cout << "Your tasks are: \n";
     for (const auto& task : tasks){
-        cout << "* " << task << "\n";
+        cout << "* " << task["title"] << " | " << task["description"] << " | " << task["due_date"] << "\n";
+    
     }
     
 }
 
+
 void clearTasks(json& tasks) {
+    
     tasks.clear();
     saveTasks(tasks);
+    cout << "All tasks cleared!\n";
+
 }
 
 void updateTasks(json& tasks){
-    cout << "Enter the name of the task you want to update: \n";
+    cout << "Enter the title of the task you want to update: \n";
     string name;
-    cin >> name;
+    getline(cin, name);
 
     for (auto& task : tasks) {
-        if (task == name) {
-            cout << "Enter the new name for the task: \n";
+        if (task["title"] == name) {
+            cout << "Enter the new title for the task: \n";
             string newName;
-            cin >> newName;
-            task = newName;
-            cout << "Task " << name << "was updated to " << newName << "\n";
+            getline(cin, newName);
+            task["title"] = newName;
+            cout << "Task " << name << " was updated to " << newName << "\n";
             break;
         }
     }
@@ -79,23 +104,24 @@ void updateTasks(json& tasks){
 
 
 // MAINNNNN
-int main()
+int main() 
 {
     json tasks = loadTasks();
     
-    cout<<"hey, Hey, HEY! WELCOME to the task manager!\n";
+    cout<<"hey, Hey, HEY! WELCOME to the task manager!\n\n";
     cout<<"1. Add a task\n";
     cout<<"2. List tasks\n";
-    cout<<"3. Update a task\n";
+    cout<<"3. Update a task title\n";
     cout<<"4. Clear tasks\n";
-    cout<<"5. Quit :(\n";
+    cout<<"5. Quit :(\n\n";
 
-    cout<<"Pick ONE! (1-5):\n";
-    int choice{};
-    cin>> choice;
-    cin.ignore();
-    
-    if (choice == 1) {
+    while (true) {
+        cout<<"Pick ONE! (1-5):\n";
+        int choice{};
+        cin>> choice;
+        cout<<"\n";
+        cin.ignore();
+        if (choice == 1) {
         addTask(tasks);
     }
     else if (choice == 2) {
@@ -112,8 +138,9 @@ int main()
         return 0;
     }
     else {
-        cout<<"Invalid choice, uh oh... try again\n";
-        cin>> choice;
+        cout<<"Invalid choice, bye\n";
+        return 0;
     }
 
+    }
 }
